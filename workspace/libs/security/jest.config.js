e7abee84e8d38e7d8ae9bb3923093ff9c56aa9fd
Matt Vaughn <matt.vaughn@buildmotion.com>
2020-09-04 18:49:57 -0600

    add workspace library projects

---
+++ workspace/libs/security/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'security',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/libs/security',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
